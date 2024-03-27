3ba246bd148a69809f1217976e25de38c84d25d6
emmyalexjnr <emmyalexjnr@gmail.com>
2024-03-27 18:52:57 +0000

    - working on login of dashboard and wishlist

--- app/src/main/java/com/broughapartments/ui/dashboard/Login.kt
+++ app/src/main/java/com/broughapartments/ui/dashboard/Login.kt
@@ -1,14 +1,16 @@
 package com.broughapartments.ui.dashboard
 
 import android.util.Log
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.text.ClickableText
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -26,6 +28,7 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.broughapartments.R
 import com.broughapartments.components.MyText
+import com.broughapartments.components.nunitoFamily
 
 @Composable
 fun Login() {
@@ -47,29 +50,45 @@ fun Login() {
             MyText(text = "Welcome back!", fontSize = 30.sp, fontWeight = FontWeight.Bold,)
             MyText(text = "Login into your account to continue", fontSize = 18.sp, modifier = Modifier.padding(0.dp, 10.dp))
         }
-        Column(modifier = Modifier.fillMaxWidth(),
+        Column(modifier = Modifier
+            .fillMaxWidth()
+            .padding(30.dp, 0.dp),
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
             OutlinedTextField(
+                modifier = Modifier
+                    .padding(0.dp, 10.dp, 0.dp, 20.dp)
+                    .fillMaxWidth(),
                 value = text,
                 onValueChange = { text = it },
-                label = { Text("Email/Username") }
+                label = { MyText("Email/Username") }
             )
 
             OutlinedTextField(
+                modifier = Modifier
+                    .padding(0.dp, 0.dp, 0.dp, 20.dp)
+                    .fillMaxWidth(),
                 value = password,
                 onValueChange = { text = it },
-                label = { Text("Password") }
+                label = { MyText("Password") },
+                textStyle = TextStyle(fontFamily = nunitoFamily)
             )
 
-            Button(onClick = {  }) {
+            Button(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(0.dp, 20.dp),
+                colors = ButtonDefaults.buttonColors(containerColor = colorResource(id = R.color.primary_color)),
+                onClick = {  }) {
                 MyText("Login")
             }
 
             Row {
-                MyText(text = "Don't have account ?")
+                MyText(
+                    modifier = Modifier.padding(10.dp, 0.dp),
+                    text = "Don't have account ?", fontSize = 16.sp)
                 ClickableText(
-                    style = TextStyle(color = colorResource(R.color.primary_color)),
+                    style = TextStyle(color = colorResource(R.color.primary_color), fontFamily = nunitoFamily, fontSize = 18.sp),
                     text = AnnotatedString("Create"),
                     onClick = { offset ->
                         Log.d("ClickableText", "$offset -th character is clicked.")
--- app/src/main/java/com/broughapartments/ui/wishlist/Wishlist.kt
+++ app/src/main/java/com/broughapartments/ui/wishlist/Wishlist.kt
@@ -5,19 +5,22 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.broughapartments.components.PropertyCard
 
 @Composable
 fun Wishlist() {
     Box(modifier = Modifier.fillMaxSize()) {
-        Column(modifier = Modifier.fillMaxSize().align(Alignment.Center),
-            verticalArrangement = Arrangement.Center,
+        Column(modifier = Modifier.fillMaxSize().align(Alignment.Center).padding(16.dp),
+//            verticalArrangement = Arrangement.Center,
             horizontalAlignment = Alignment.CenterHorizontally) {
-            Text(text = "Wishlist", fontSize = 30.sp)
+            PropertyCard()
         }
     }
 }
